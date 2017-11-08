a35fe60364444cb195fd21bce026c40147f14da7
161707S LEE MING HAN <161707S@sit.nyp.edu.sg>
2017-11-08 11:23:55 +0800

    first commit

--- MathAdd.java
+++ MathAdd.java
@@ -4,5 +4,6 @@ public class MathAdd {
 	
 	static public int add(int a, int b) {   
         return a + b; 
+        System.out.print("Added a and b");
 	}
 }
---
+++ MathSub.java
@@ -0,0 +1,8 @@
+public class MathSub {
+	
+	public MathSub {}
+	
+	static public int sub(int a, int b) {   
+        return a - b; 
+	}
+}
